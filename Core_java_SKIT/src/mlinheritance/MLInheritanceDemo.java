637a97f7fdf9f3cc4d5ff5b14b7a293af1ed1a72
ml-akash <mlakash852@gmail.com>
2026-04-30 22:55:52 +0530

    first commit

--- Core_java_SKIT/src/mlinheritance/MLInheritanceDemo.java
+++ Core_java_SKIT/src/mlinheritance/MLInheritanceDemo.java
@@ -1,10 +1,9 @@
 package mlinheritance;
 
-public class MLinheritanceDemo {
+public class MLInheritanceDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-
 }
