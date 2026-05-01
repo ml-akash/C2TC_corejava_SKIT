67fc922993edf2fd00d5506dac4b61f159118eb9
ml-akash <mlakash852@gmail.com>
2026-05-01 07:57:38 +0530

    first commit

---
+++ Core_java_SKIT/src/com/tnsif/polymorphism/Point.java
@@ -0,0 +1,23 @@
+package com.tnsif.polymorphism;
+
+public class Point {
+	 private float x;
+	 private float y;
+	 
+	public Point() {
+		this.x=0;
+		this.y=0;
+	}
+	public Point(float x) {
+		this.x = x;
+		this.y= x;
+	}
+	public Point(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+	@Override
+	public String toString() {
+		return "Point [x=" + x + ", y=" + y + "]";
+	}
+	}
